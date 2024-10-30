227b3e897dc749038fdd2b0d7d43d4e4a08bd8bb
Alex Nikonov <60483531+lexxA87@users.noreply.github.com>
2024-10-30 15:48:16 +0400

    Added Two classes Director and Movie with OnToMany

---
+++ src/main/java/org/example/model/Movie.java
@@ -0,0 +1,70 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movie")
+public class Movie {
+
+    @Id
+    @Column(name = "movie_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int movie_id;
+
+    @ManyToOne
+    @JoinColumn(name = "director_id", referencedColumnName = "director_id")
+    private Director director;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "year_of_production")
+    private int yearOfProduction;
+
+    public Movie() {}
+
+    public Movie(String name, int yearOfProduction) {
+        this.name = name;
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public int getId() {
+        return movie_id;
+    }
+
+    public void setId(int id) {
+        this.movie_id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public void setYearOfProduction(int yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "name='" + name + '\'' +
+                ", yearOfProduction=" + yearOfProduction +
+                '}';
+    }
+}
